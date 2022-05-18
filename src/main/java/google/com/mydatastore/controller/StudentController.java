05f1cf7b05a85c45d9e803967a7ad0b2fd1a26f3
Indeshwar <indeshc@gmail.com>
2022-05-18 16:33:29 -0500

    updated commit

--- src/main/java/google/com/mydatastore/controller/StudentController.java
+++ src/main/java/google/com/mydatastore/controller/StudentController.java
@@ -3,6 +3,7 @@ package google.com.mydatastore.controller;
 import google.com.mydatastore.model.Student;
 import google.com.mydatastore.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class StudentController {
     @PostMapping("/save")
     public void saveStudent(@RequestBody Student student) throws IOException {
         studentService.createStudent(student);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello GCP!";
     }
 }
